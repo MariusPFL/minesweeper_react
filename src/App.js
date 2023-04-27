527bbc701f5ea346c929080e8ff491ada1880822
Marius Pfluger <marius.pfluger@autismuslink.ch>
2023-04-27 15:49:24 +0200

    Field Creation by Automatically

--- src/App.js
+++ src/App.js
@@ -13,18 +13,11 @@ function App() {
   let columnLength = 5;
   // Ist hier die yAxis
   let rowLength = 5;
-  let bombsCount;
+  const [bombsCount, setBombsCount] = React.useState(localStorage.getItem("bombsNumber") != null ? localStorage.getItem("bombsNumber") : rowLength);
   let gameFieldAsArray = [];
   
   const [openFields, setOpenFields] = React.useState(0);
   const [showWinEffect, setShowWinEffect] = React.useState(false);
-  // Get things from the LocalStorage
-  if (localStorage.getItem("bombsNumber") != null) {
-    bombsCount = localStorage.getItem("bombsNumber")
-  }
-  else{
-    bombsCount = rowLength;
-  }
 
   let resultFromLocalStorage = JSON.parse(localStorage.getItem("gameField"));
   console.log("Results from Local Storage:")
@@ -36,6 +29,10 @@ function App() {
     CreateFieldAndSaveToLocalStorage();
   }
 
+  // Intizalizing UseEffect
+  React.useEffect(() => {
+    createFields();
+  }, [])
 
   // Check winning Conditions
   React.useEffect(() => {
@@ -95,7 +92,8 @@ function App() {
       }
     }
   }
-
+  
+  
   function setNumber(yAxisIndex, xAxisIndex){
     console.log('setting for: y: ' + yAxisIndex + ' x: ' + xAxisIndex)
     if (yAxisIndex < 0 || yAxisIndex > rowLength -1) {
@@ -115,151 +113,165 @@ function App() {
   function getRandomNumber(max){
     return Math.floor(Math.random() * max);
   }
+  
+  function showTutorial(){
+    alert("Your objective is to click all the hidden fields [?] without ever clicking on a mine. You can (un)mark the fields with Holding CTRL while clicking! If you wanna make your life easier or more hell ;) you can adjust the difficulty very precisely down below, adjust the field and the Bombscount and then you can click on regenerate to save it and generate.")
+  }
 
-  // function createFields(){
-  //   for (let rowCount = 0; rowCount < rowLength; rowCount++) {
-  //     for (let columnCount = 0; columnCount < columnLength; columnCount++) {
-  //       (
-  //         <Field 
-  //         id = {((rowCount * 10) + "|" + (columnCount * 10))}
-  //         value = {gameFieldAsArray[rowCount][columnCount]}
-  //         />
-  //       )
-  //     }
-  //   }
-  // }
+  function createFields(){
+    return(
+      gameFieldAsArray.map((row, yIndex) => (
+        <div className='rowContainer'>
+          {
+            row.map((fieldValue, xIndex) => (
+            <Field 
+              value = {gameFieldAsArray[yIndex][xIndex]}
+              increaseCounterMethod = {increaseOpenedFieldsCounter}
+            />
+            ))
+          }
+          </div>
+      ))
+    )
+  }
+
   return (
     <div className="App">
       {showWinEffect ? <Confetti /> : "" }
       <div className='blankSpaces100px'/>
       <div className='gameboy'>
-        <div className='gameField'>
-          {
-            // createFields()
-          }
-          <div className='rowContainer'>
-          <Field 
-            value = {gameFieldAsArray[0][0]}
-            increaseCounterMethod = {increaseOpenedFieldsCounter}
-          />
-          <Field 
-            value = {gameFieldAsArray[0][1]}
-            increaseCounterMethod = {increaseOpenedFieldsCounter}
-          />
-          <Field 
-            value = {gameFieldAsArray[0][2]}
-            increaseCounterMethod = {increaseOpenedFieldsCounter}
-          />
-          <Field 
-            value = {gameFieldAsArray[0][3]}
-            increaseCounterMethod = {increaseOpenedFieldsCounter}
-          />
-          <Field 
-            value = {gameFieldAsArray[0][4]}
-            increaseCounterMethod = {increaseOpenedFieldsCounter}
-          />
-          </div>
+        <center>
+          <div className='gameField' id='gameField'>
+            {
+              createFields()
+            }
+            {/* <div className='rowContainer'>
+            <Field 
+              value = {gameFieldAsArray[0][0]}
+              increaseCounterMethod = {increaseOpenedFieldsCounter}
+            />
+            <Field 
+              value = {gameFieldAsArray[0][1]}
+              increaseCounterMethod = {increaseOpenedFieldsCounter}
+            />
+            <Field 
+              value = {gameFieldAsArray[0][2]}
+              increaseCounterMethod = {increaseOpenedFieldsCounter}
+            />
+            <Field 
+              value = {gameFieldAsArray[0][3]}
+              increaseCounterMethod = {increaseOpenedFieldsCounter}
+            />
+            <Field 
+              value = {gameFieldAsArray[0][4]}
+              increaseCounterMethod = {increaseOpenedFieldsCounter}
+            />
+            </div>
 
-          <div className='rowContainer'>
-            <Field 
-              value = {gameFieldAsArray[1][0]}
-              increaseCounterMethod = {increaseOpenedFieldsCounter}
-            />
-            <Field 
-              value = {gameFieldAsArray[1][1]}
-              increaseCounterMethod = {increaseOpenedFieldsCounter}
-            />
-            <Field 
-              value = {gameFieldAsArray[1][2]}
-              increaseCounterMethod = {increaseOpenedFieldsCounter}
-            />
-            <Field 
-              value = {gameFieldAsArray[1][3]}
-              increaseCounterMethod = {increaseOpenedFieldsCounter}
-            />
-            <Field 
-              value = {gameFieldAsArray[1][4]}
-              increaseCounterMethod = {increaseOpenedFieldsCounter}
-            />
-          </div>
+            <div className='rowContainer'>
+              <Field 
+                value = {gameFieldAsArray[1][0]}
+                increaseCounterMethod = {increaseOpenedFieldsCounter}
+              />
+              <Field 
+                value = {gameFieldAsArray[1][1]}
+                increaseCounterMethod = {increaseOpenedFieldsCounter}
+              />
+              <Field 
+                value = {gameFieldAsArray[1][2]}
+                increaseCounterMethod = {increaseOpenedFieldsCounter}
+              />
+              <Field 
+                value = {gameFieldAsArray[1][3]}
+                increaseCounterMethod = {increaseOpenedFieldsCounter}
+              />
+              <Field 
+                value = {gameFieldAsArray[1][4]}
+                increaseCounterMethod = {increaseOpenedFieldsCounter}
+              />
+            </div>
 
 
-          <div className='rowContainer'>
-            <Field 
-              value = {gameFieldAsArray[2][0]}
-              increaseCounterMethod = {increaseOpenedFieldsCounter}
-            />
-            <Field 
-              value = {gameFieldAsArray[2][1]}
-              increaseCounterMethod = {increaseOpenedFieldsCounter}
-            />
-            <Field 
-              value = {gameFieldAsArray[2][2]}
-              increaseCounterMethod = {increaseOpenedFieldsCounter}
-            />
-            <Field 
-              value = {gameFieldAsArray[2][3]}
-              increaseCounterMethod = {increaseOpenedFieldsCounter}
-            />
-            <Field 
-              value = {gameFieldAsArray[2][4]}
-              increaseCounterMethod = {increaseOpenedFieldsCounter}
-            />
-          </div>
+            <div className='rowContainer'>
+              <Field 
+                value = {gameFieldAsArray[2][0]}
+                increaseCounterMethod = {increaseOpenedFieldsCounter}
+              />
+              <Field 
+                value = {gameFieldAsArray[2][1]}
+                increaseCounterMethod = {increaseOpenedFieldsCounter}
+              />
+              <Field 
+                value = {gameFieldAsArray[2][2]}
+                increaseCounterMethod = {increaseOpenedFieldsCounter}
+              />
+              <Field 
+                value = {gameFieldAsArray[2][3]}
+                increaseCounterMethod = {increaseOpenedFieldsCounter}
+              />
+              <Field 
+                value = {gameFieldAsArray[2][4]}
+                increaseCounterMethod = {increaseOpenedFieldsCounter}
+              />
+            </div>
 
-          <div className='rowContainer'>
-            <Field 
-              value = {gameFieldAsArray[3][0]}
-              increaseCounterMethod = {increaseOpenedFieldsCounter}
-            />
-            <Field 
-              value = {gameFieldAsArray[3][1]}
-              increaseCounterMethod = {increaseOpenedFieldsCounter}
-            />
-            <Field 
-              value = {gameFieldAsArray[3][2]}
-              increaseCounterMethod = {increaseOpenedFieldsCounter}
-            />
-            <Field 
-              value = {gameFieldAsArray[3][3]}
-              increaseCounterMethod = {increaseOpenedFieldsCounter}
-            />
-            <Field 
-              value = {gameFieldAsArray[3][4]}
-              increaseCounterMethod = {increaseOpenedFieldsCounter}
-            />
-          </div>
+            <div className='rowContainer'>
+              <Field 
+                value = {gameFieldAsArray[3][0]}
+                increaseCounterMethod = {increaseOpenedFieldsCounter}
+              />
+              <Field 
+                value = {gameFieldAsArray[3][1]}
+                increaseCounterMethod = {increaseOpenedFieldsCounter}
+              />
+              <Field 
+                value = {gameFieldAsArray[3][2]}
+                increaseCounterMethod = {increaseOpenedFieldsCounter}
+              />
+              <Field 
+                value = {gameFieldAsArray[3][3]}
+                increaseCounterMethod = {increaseOpenedFieldsCounter}
+              />
+              <Field 
+                value = {gameFieldAsArray[3][4]}
+                increaseCounterMethod = {increaseOpenedFieldsCounter}
+              />
+            </div>
 
-          <div className='rowContainer'>
-            <Field 
-              value = {gameFieldAsArray[4][0]}
-              increaseCounterMethod = {increaseOpenedFieldsCounter}
-            />
-            <Field 
-              value = {gameFieldAsArray[4][1]}
-              increaseCounterMethod = {increaseOpenedFieldsCounter}
-            />
-            <Field 
-              value = {gameFieldAsArray[4][2]}
-              increaseCounterMethod = {increaseOpenedFieldsCounter}
-            />
-            <Field 
-              value = {gameFieldAsArray[4][3]}
-              increaseCounterMethod = {increaseOpenedFieldsCounter}
-            />
-            <Field 
-              value = {gameFieldAsArray[4][4]}
-              increaseCounterMethod = {increaseOpenedFieldsCounter}
-            />
+            <div className='rowContainer'>
+              <Field 
+                value = {gameFieldAsArray[4][0]}
+                increaseCounterMethod = {increaseOpenedFieldsCounter}
+              />
+              <Field 
+                value = {gameFieldAsArray[4][1]}
+                increaseCounterMethod = {increaseOpenedFieldsCounter}
+              />
+              <Field 
+                value = {gameFieldAsArray[4][2]}
+                increaseCounterMethod = {increaseOpenedFieldsCounter}
+              />
+              <Field 
+                value = {gameFieldAsArray[4][3]}
+                increaseCounterMethod = {increaseOpenedFieldsCounter}
+              />
+              <Field 
+                value = {gameFieldAsArray[4][4]}
+                increaseCounterMethod = {increaseOpenedFieldsCounter}
+              />
+            </div> */}
           </div>
-        </div>
+        </center>
         <p>Opened Fields: {openFields}</p>
+        <p>Fields left: {rowLength * columnLength - openFields}</p>
         <p>Bombs count: {bombsCount}</p>
+        <button onClick={showTutorial}>?</button>
         <button onClick={RestartTheGame}>Restart</button>
-        <input type='number' value={bombsCount} id="inputBombNumber"/>
+        <input type='number' value={bombsCount} onChange={(event) => {setBombsCount(event.target.value)}} id="inputBombNumber" min={1} max={rowLength * columnLength / 2}/>
         <button onClick={() =>{
-          localStorage.setItem("bombsNumber", document.getElementById("inputBombNumber").value)
-        }}>regenerate</button>
+          localStorage.setItem("bombsNumber", bombsCount)
+          RestartTheGame();
+        }}>regenerate Field</button>
       </div>
     </div>
   );
